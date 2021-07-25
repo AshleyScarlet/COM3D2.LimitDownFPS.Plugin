a79b1a809784232f4870652078b02e4cd3a3e413
Ashley Scarlet <70315656+AshleyScarlet@users.noreply.github.com>
2021-07-25 22:12:44 +0900

    プロジェクト ファイルを追加します。

---
+++ COM3D2.LimitDownFPS.Plugin/Util.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.InteropServices;
+using System.Text;
+using UnityEngine;
+
+namespace COM3D2.LimitDownFPS.Plugin
+{
+    internal static class Util
+    {
+        public static int GetPixel(int value)
+        {
+            float num = 1f + (Screen.width / 1280f - 1f) * 0.6f;
+            return (int)(num * value);
+        }
+
+        [DllImport("kernel32.dll")]
+        public static extern uint GetPrivateProfileString(string lpAppName, string lpKeyName, string lpDefault, StringBuilder lpReturnedString, uint nSize, string lpFileName);
+
+        [DllImport("kernel32.dll")]
+        public static extern uint WritePrivateProfileString(string lpAppName, string lpKeyName, string lpString, string lpFileName);
+    }
+}
